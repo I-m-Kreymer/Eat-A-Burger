c9e9f7287431f14066e2d8da34bdaca3b1f0b157
Igor Kreymer <igorkr5@gmail.com>
2017-03-20 21:39:49 -0400

    Working app without styling

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BigMac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Stuffed Jalapeno burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegie Burger', FALSE);
